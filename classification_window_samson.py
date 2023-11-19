3b8faf9f78cd10ad0ffaad2fa6fe9ce2de498ed7
Nitesh Kumar Singh <82447589+Ns103213@users.noreply.github.com>
2023-11-20 01:23:51 +0200

    classification_window_samson.py

--- classification_window_samson.py
+++ classification_window_samson.py
@@ -45,7 +45,7 @@ class ClassificationWindowSamson:
 
         ttk.Label(text="").pack()  # just for spacing.
 #         #Add norway logo.
-        self.window.iconbitmap(os.path.join("resources", 'elo-hyp_logo.ico'))
+        self.window.iconbitmap(os.path.join("Resources", 'elo-hyp_logo.ico'))
 
         
     def __get_classification_window3(self):
